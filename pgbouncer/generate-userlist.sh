369ef8631fccf8439b7e7deac8a2f690029bc57d
Anashkin Aleksei <109337222+NewalexOA@users.noreply.github.com>
2024-12-01 20:19:41 +0300

    feat(pgBouncer): update configuration and enhance monitoring
    - Added healthcheck for pgBouncer in `docker-compose.yml`
    - Updated `generate-userlist.sh` script to generate pgBouncer ini file using a template
    - Refactored `pgbouncer.ini`:
      - Added `auth_query` configuration to integrate with PostgreSQL
      - Adjusted `server_reset_query` and `ignore_startup_parameters` for improved compatibility
    - Enhanced monitoring in `monitor_pools.sh`:
      - Rewritten pool monitoring using a multi-line SQL query (EOF)
    
    Technical details:
    - Implemented a healthcheck using the `psql` command to verify pgBouncer status
    - Introduced user management with md5 hashed credentials in the userlist
    - Simplified connection management and monitoring through `psql`
    - Improved readability and reduced redundant code in scripts

--- pgbouncer/generate-userlist.sh
+++ pgbouncer/generate-userlist.sh
@@ -3,3 +3,8 @@
 
 PGPASSWORD_HASH=$(echo -n "${POSTGRES_PASSWORD}${POSTGRES_USER}" | md5sum | cut -d' ' -f1)
 echo "\"${POSTGRES_USER}\" \"md5${PGPASSWORD_HASH}\"" > /etc/pgbouncer/userlist.txt
+
+# Generate pgbouncer.ini from template
+cp /etc/pgbouncer/pgbouncer.ini /etc/pgbouncer/pgbouncer.ini.tmp
+sed "s/\${POSTGRES_USER}/${POSTGRES_USER}/g" /etc/pgbouncer/pgbouncer.ini.tmp > /etc/pgbouncer/pgbouncer.ini
+rm /etc/pgbouncer/pgbouncer.ini.tmp
--- scripts/monitor_pools.sh
+++ scripts/monitor_pools.sh
@@ -2,16 +2,20 @@
 
 while true; do
     echo "=== $(date) ==="
-    echo "=== POOLS ==="
-    PGPASSWORD=${POSTGRES_PASSWORD} psql -h pgbouncer -p 6432 -U ${POSTGRES_USER} -d pgbouncer -c "SHOW POOLS;"
-    echo "=== CLIENTS ==="
-    PGPASSWORD=${POSTGRES_PASSWORD} psql -h pgbouncer -p 6432 -U ${POSTGRES_USER} -d pgbouncer -c "SHOW CLIENTS;"
-    echo "=== SERVERS ==="
-    PGPASSWORD=${POSTGRES_PASSWORD} psql -h pgbouncer -p 6432 -U ${POSTGRES_USER} -d pgbouncer -c "SHOW SERVERS;"
-    echo "=== STATS ==="
-    PGPASSWORD=${POSTGRES_PASSWORD} psql -h pgbouncer -p 6432 -U ${POSTGRES_USER} -d pgbouncer -c "SHOW STATS;"
-    echo "=== LISTS ==="
-    PGPASSWORD=${POSTGRES_PASSWORD} psql -h pgbouncer -p 6432 -U ${POSTGRES_USER} -d pgbouncer -c "SHOW LISTS;"
+    
+    PGPASSWORD=${POSTGRES_PASSWORD} psql -t -h pgbouncer -p 6432 -U wallet_user pgbouncer <<EOF
+    \echo === POOLS ===
+    SHOW POOLS;
+    \echo === STATS ===
+    SHOW STATS;
+    \echo === LISTS ===
+    SHOW LISTS;
+    \echo === CLIENTS ===
+    SHOW CLIENTS;
+    \echo === SERVERS ===
+    SHOW SERVERS;
+EOF
+    
     echo "==================="
     sleep 5
 done 
